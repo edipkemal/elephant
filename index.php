8d058f949ade55fd5eff28560b182664a1e602af
Edip Sardoğan <edipkemalsardogan09@gmail.com>
2022-06-07 01:04:07 +0300

    style.css update

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ header("Refresh: 2; URL=$url");
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="icon" href="nature.png" />
     <style>
       @import url("https://fonts.googleapis.com/css2?family=Lobster&display=swap");
